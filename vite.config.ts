10c41adb8bb9bf349274d16323e9cb0ed03aaf2b
ZikAsakur <khairutdinovraushan@gmail.com>
2025-11-16 18:47:02 +0300

    Попытка 3

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Argonomist_Garden/',
   plugins: [react()],
    server: {
     proxy:{
